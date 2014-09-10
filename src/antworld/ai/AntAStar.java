8451c88b4e454a8e03b147e491276cd1cd7a1cd2
esosebee <esosebee@users.noreply.github.com>
2014-09-10 12:04:03 -0600

    Not quite finished
    Still a standalone class; not implemented into the project yet

--- src/antworld/ai/AntAStar.java
+++ src/antworld/ai/AntAStar.java
@@ -1,24 +1,15 @@
 package antworld.ai;
 
-import java.awt.*;
-import java.applet.*;
+/**
+* @author 	Erin Sosebee
+* @version	2014.09.09
+*/
+
 import java.util.ArrayList;
 
-// TODO: This class was originally using the ListNode and PathNode classes, 
-// 			 but there were weird errors, so for now, every single parameter for
-//			 the nodes is being represented as its own separate ArrayList. I would
-//			 like to change this for the future. But for now, here's an program 
-//			 that calculates the shortest path to a destination, and draws it on
-//			 an applet screen (we should probably not have this run on an applet,
-//			 so that's another future change).
-
-// For now, this class is just a test.
-
-// NOTE: The path changes every time you interact with the applet window.
-
-public class AntAStar extends Applet
+public class AntAStar
 {
-	// TODO: Currently uses this board that I created. Need to load provided map instead
+	// Currently uses this board that I created. Need to load provided map instead
 	int board[][] =
 		{
 			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -41,62 +32,58 @@ public class AntAStar extends Applet
 
 	int startX, startY, destX, destY;
 
-	// TODO: Make these ArrayLists of nodes instead
-	// ArrayList<ListNode> open
-	// Open list ( x, y, f, g, h, parentx, parenty )
-	ArrayList<Integer> openX = new ArrayList<Integer>();
-	ArrayList<Integer> openY = new ArrayList<Integer>();
-	ArrayList<Integer> openF = new ArrayList<Integer>();
-	ArrayList<Integer> openG = new ArrayList<Integer>();
-	ArrayList<Integer> openH = new ArrayList<Integer>();
-	ArrayList<Integer> openPx = new ArrayList<Integer>();
-	ArrayList<Integer> openPy = new ArrayList<Integer>();
+	// Open/closed lists (x, y, f, g, h, parentX, parentY)
+	ArrayList<Node> open = new ArrayList<Node>();
+	ArrayList<Node> closed = new ArrayList<Node>();
 	
-	// TODO: ArrayList<ListNode> closed
-	// Closed list ( x, y, f, g, h, parentx, parenty )
-	ArrayList<Integer> closedX = new ArrayList<Integer>();
-	ArrayList<Integer> closedY = new ArrayList<Integer>();
-	ArrayList<Integer> closedF = new ArrayList<Integer>();
-	ArrayList<Integer> closedG = new ArrayList<Integer>();
-	ArrayList<Integer> closedH = new ArrayList<Integer>();
-	ArrayList<Integer> closedPx = new ArrayList<Integer>();
-	ArrayList<Integer> closedPy = new ArrayList<Integer>();
-	
-	// TODO: ArrayLIst<PathNode> path
-	// Path
-	ArrayList<Integer> pathX = new ArrayList<Integer>();
-	ArrayList<Integer> pathY = new ArrayList<Integer>();
+	// Path (x, y)
+	ArrayList<Node> path = new ArrayList<Node>();
 
-	/**
-	 * Initialize applet window
-	 */
-	public void init()
+	// Constructor
+	public AntAStar(int startX, int startY, int destX, int destY)
 	{
-		setBackground(new Color(155, 205, 155));
+		this.startX = startX;
+		this.startY = startY;
+		this.destX = destX;
+		this.destY = destY;
+//		System.out.println(startX + "," + startY + "," + destX + "," + destY);
+		findPath(startX, startY, destX, destY);
 	}
 
 	/**
-	 * Once the destination has been reached, builds the path based on the 
-	 * nodes present in the closed list.
+	 * Once the destination has been reached, the path is built based on which
+	 * nodes are present in the closed list.
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
 	 */
-	public void traversePath()
+	public void traversePath(int x1, int y1, int x2, int y2)
 	{
 		boolean exit = false;
-		int x = destX;
-		int y = destY;
+		int startX = x1;
+		int startY = y1;
+		int endX = x2;
+		int endY = y2;
+		
 		while (exit == false)
 		{
-			for (int i = 0; i < closedX.size(); i++)
+			for (int i = 0; i < closed.size(); i++)
 			{
-				if (closedX.get(i) == x && closedY.get(i) == y)
+//				System.out.println(closed.get(i).getX() + "," + closed.get(i).getY());
+				if (closed.get(i).getX() == endX && closed.get(i).getY() == endY)
 				{
-					x = closedPx.get(i);
-					y = closedPy.get(i);
-					pathX.add(x);
-					pathY.add(y);
+//					System.out.println("found the end");
+					endX = closed.get(i).getParentX();
+					endY = closed.get(i).getParentY();
+//					System.out.println("endX: " + endX);
+//					System.out.println("endY: " + endY);
+					path.add(new Node(endX, endY));
 				}
+//				System.out.println();
 			}
-			if (x == startX && y == startY)
+			if (endX == startX && endY == startY)
 			{
 				exit = true;
 			}
@@ -105,34 +92,37 @@ public class AntAStar extends Applet
 
 	/**
 	 * Removes a node from the open list.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return true if a node has been removed, else false.
 	 */
 	public boolean removeFromOpen(int x, int y)
 	{
-		for (int i = 0; i < openX.size(); i++)
+		for (int i = 0; i < open.size(); i++)
 		{
-			if (openX.get(i) == x && openY.get(i) == y)
+			if (open.get(i).getX() == x && open.get(i).getY() == y)
 			{
-				openX.remove(i);
-				openY.remove(i);
-				openF.remove(i);
-				openG.remove(i);
-				openH.remove(i);
-				openPx.remove(i);
-				openPy.remove(i);
+				open.remove(i);
 				return true;
 			}
 		}
 		return false;
 	}
 
-	/**
-	 * Checks if a node is in the closed list.
-	 */
+/**
+* Checks if a point is contained within the list of 'closed'
+* nodes.
+* 
+* @param x
+* @param y
+* @return true if the given point is in the closed list, otherwise false.
+*/
 	public boolean inClosed(int x, int y)
 	{
-		for (int i = 0; i < closedX.size(); i++)
+		for (int i = 0; i < closed.size(); i++)
 		{
-			if (closedX.get(i) == x && closedY.get(i) == y)
+			if (closed.get(i).getX() == x && closed.get(i).getY() == y)
 			{
 				return true;
 			}
@@ -140,14 +130,18 @@ public class AntAStar extends Applet
 		return false;
 	}
 
-	/**
-	 * Checks if the given point is in the open list.
-	 */
+/**
+* Determines whether or not a point is found within the 'open'
+* list of nodes.
+* 
+* @param x
+* @param y
+* @return true if the given point is in the open list, otherwise false.
+*/
 	public boolean inOpen(int x, int y)
-	{
-		for (int i = 0; i < openX.size(); i++)
+	{		for (int i = 0; i < open.size(); i++)
 		{
-			if (openX.get(i) == x && openY.get(i) == y)
+			if (open.get(i).getX() == x && open.get(i).getY() == y)
 			{
 				return true;
 			}
@@ -155,43 +149,26 @@ public class AntAStar extends Applet
 		return false;
 	}
 
-	/**
-	 * Checks if the open list is empty.
-	 */
+/**
+* Checks if there are any nodes in the open list.
+* 
+* @return true if the list is empty, otherwise false.
+*/
 	public boolean emptyOpen()
 	{
-		if (openX.size() > 0)
-		{
-			return false;
-		}
+		if (open.size() > 0) return false;
 		return true;
 	}
 
 	/**
-	 * Sets the start and end coordinates (random for now).
-	 */
-	public void setCoordinates()
-	{
-		boolean exit = false;
-		while (exit == false)
-		{
-			startX = (int) (Math.random() * mapWidth);
-			startY = (int) (Math.random() * mapHeight);
-			destX = (int) (Math.random() * mapWidth);
-			destY = (int) (Math.random() * mapHeight);
-			if (board[startY][startX] == 0 && board[destY][destX] == 0)
-			{
-				if (startX != destX && startY != destY)
-				{
-					exit = true;
-				}
-			}
-		}
-	}
-
-	/**
-	 * Gets the position of a point within walkable distance of the current point.
-	 * Adds the point onto the open list.
+	 * Gets the position of a point within reachable distance of the current
+	 * point. Adds the point onto the open list.
+	 * 
+	 * @param newX
+	 * @param newY
+	 * @param tempX
+	 * @param tempY
+	 * @param tempG
 	 */
 	public void getPosition(int newX, int newY, int tempX, int tempY, int tempG)
 	{
@@ -204,13 +181,10 @@ public class AntAStar extends Applet
 				{
 					if (board[newY][newX] == 0)
 					{
-						openX.add(newX);
-						openY.add(newY);
-						openG.add(tempG + 1);
-						openH.add(getDistance(newX, newY, destX, destY));
-						openF.add((tempG + 1) + getDistance(newX, newY, destX, destY));
-						openPx.add(tempX);
-						openPy.add(tempY);
+						int newG = tempG + 1;
+						int newH = getDistance(newX, newY, destX, destY);
+						int newF = newG + newH;
+						open.add(new Node(newX, newY, newG, newH, newF, tempX, tempY));
 					}
 				}
 			}
@@ -218,120 +192,96 @@ public class AntAStar extends Applet
 	}
 	
 	/**
+	 * Calculates the shortest path between two points using the A* path finding
+	 * algorithm.
 	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
 	 */
-	public void findPath()
+	public void findPath(int x1, int y1, int x2, int y2)
 	{
 		// Clear all the lists and the path data
-		openX.clear();
-		openY.clear();
-		openF.clear();
-		openG.clear();
-		openH.clear();
-		openPx.clear();
-		openPy.clear();
-
-		closedX.clear();
-		closedY.clear();
-		closedF.clear();
-		closedG.clear();
-		closedH.clear();
-		closedPx.clear();
-		closedPy.clear();
-
-		pathX.clear();
-		pathY.clear();
+		open.clear();
+		closed.clear();
+		path.clear();
 
 		// Place the start position onto the open list
-		openX.add(startX);
-		openY.add(startY);
-		openF.add(0);
-		openG.add(0);
-		openH.add(0);
-		openPx.add(0);
-		openPy.add(0);
-
-		// Initialize temp values
+		open.add(new Node(x1, y1, 0, 0, 0, 0, 0));
+		
+		// Initialize temp/comparison values
 		boolean exit = false;
-		int tempX = 0;
-		int tempY = 0;
-		int tempF = 0;
-		int tempG = 0;
-		int tempH = 0;
-		int tempPx = 0;
-		int tempPy = 0;
-
+		int tempX = 0, tempY = 0; 
+		int tempF = 0, tempG = 0, tempH = 0;
+		int tempPx = 0, tempPy = 0;
 		int minFCost = 0;
 
 		while (exit == false)
 		{
 			// Exit the loop if the open list is empty
-			if (emptyOpen() == true)
-			{
-				exit = true;
-			}
+			if (emptyOpen() == true) exit = true;
 			
-			minFCost = Integer.MAX_VALUE; // Set the F cost to arbitrary large number for comparison
-			for (int i = 0; i < openX.size(); i++)
+			// Set the F cost to arbitrary large number for sake of comparison.
+			minFCost = Integer.MAX_VALUE; 
+			
+			// Finds which point has the lowest F score.
+			for (int i = 0; i < open.size(); i++)
 			{
-				if (openF.get(i) < minFCost)
+				if (open.get(i).getF() < minFCost)
 				{
-					minFCost = openF.get(i);
-					tempX = openX.get(i);
-					tempY = openY.get(i);
-					tempF = openF.get(i);
-					tempG = openG.get(i);
-					tempH = openH.get(i);
-					tempPx = openPx.get(i);
-					tempPy = openPy.get(i);
+					minFCost = open.get(i).getF();
+					tempX = open.get(i).getX();
+					tempY = open.get(i).getY();
+					tempG = open.get(i).getG();
+					tempH = open.get(i).getH();
+					tempPx = open.get(i).getParentX();
+					tempPy = open.get(i).getParentY();
 				}
 			}
-
+//			System.out.println(minFCost);
 			// If the current point is the target location, then the path has 
 			// been found.
 			if (tempX == destX && tempY == destY)
 			{
 				exit = true;
-				closedX.add(tempX);
-				closedY.add(tempY);
-				closedF.add(tempF);
-				closedG.add(tempG);
-				closedH.add(tempH);
-				closedPx.add(tempPx);
-				closedPy.add(tempPy);
-				traversePath();
+				closed.add(new Node(tempX, tempY, tempF, tempG, tempH, 
+						tempPx, tempPy));
+				
+				// Get path to be traveled
+				traversePath(x1, y1, x2, y2);
 			}
 			else
 			{
 				// Move the current position onto the closed list
-				closedX.add(tempX);
-				closedY.add(tempY);
-				closedF.add(tempF);
-				closedG.add(tempG);
-				closedH.add(tempH);
-				closedPx.add(tempPx);
-				closedPy.add(tempPy);
-				
+				closed.add(new Node(tempX, tempY, tempF, tempG, tempH, tempPx, tempPy));
+
 				// Remove the current position from the open list
 				removeFromOpen(tempX, tempY);
 				
 				// Get the eight positions around the current point
 				// Move them onto the open list
-				getPosition(tempX, tempY + 1, tempX, tempY, tempG); // Up
-				getPosition(tempX, tempY - 1, tempX, tempY, tempG); // Down
-				getPosition(tempX - 1, tempY, tempX, tempY, tempG); // Left
-				getPosition(tempX + 1, tempY, tempX, tempY, tempG); // Right
-				getPosition(tempX - 1, tempY + 1, tempX, tempY, tempG); // Left up diagonal
-				getPosition(tempX - 1, tempY - 1, tempX, tempY, tempG); // Left down diagonal
-				getPosition(tempX + 1, tempY + 1, tempX, tempY, tempG); // Right up diagonal
-				getPosition(tempX + 1, tempY - 1, tempX, tempY, tempG); // Right down diagonal
+				getPosition(tempX, tempY + 1, tempX, tempY, tempG); // North
+				getPosition(tempX, tempY - 1, tempX, tempY, tempG); // South
+				getPosition(tempX - 1, tempY, tempX, tempY, tempG); // West
+				getPosition(tempX + 1, tempY, tempX, tempY, tempG); // East
+				getPosition(tempX - 1, tempY + 1, tempX, tempY, tempG); // Northwest
+				getPosition(tempX - 1, tempY - 1, tempX, tempY, tempG); // Southwest
+				getPosition(tempX + 1, tempY + 1, tempX, tempY, tempG); // Northeast
+				getPosition(tempX + 1, tempY - 1, tempX, tempY, tempG); // Southeast
 			}
 		}
 	}
-
+	
 	/**
-	 * Calculates the distance using the equation:
+	 * Calculates the distance between two points using the formula:
 	 * d = sqrt((x1 - x2)^2 + (y1 - y2)^2)
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return an int representing the distance between two points.
 	 */
 	public int getDistance(int x1, int y1, int x2, int y2)
 	{
@@ -341,42 +291,24 @@ public class AntAStar extends Applet
 	}
 
 	/**
-	 * Draws the board, the start and destination points, and the path  on the applet screen.
-	 * This method is necessary for using the applet.
+	 * 
 	 */
-	public void paint(Graphics g)
+	public void getPath()
 	{
-		setCoordinates();
-		findPath();
-		
-		// DRAWING THE BOARD	
-		// Draw the obstacles
-		g.setColor(new Color(46, 139, 87));
-		for (int y = 0; y < mapHeight; y++)
+		System.out.println("path: ");
+		for (int i = 0; i < path.size(); i++)
 		{
-			for (int x = 0; x < mapWidth; x++)
-			{
-				if (board[y][x] == 1)
-				{
-					g.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
-				}
-			}
+			// TODO: do stuff with the path here
+			// note: the path only contains the points between the first and the last
+			System.out.println(path.get(i).getX() + "," + path.get(i).getY());
 		}
-		
-		// Draw the start position
-		g.setColor(Color.GREEN);
-		g.setColor(new Color(0, 0, 238));
-		g.fillOval(startX * cellWidth + 4, startY * cellHeight + 4, 8, 8);
-		
-		// Draw the destination position
-		g.setColor(new Color(255, 48, 48));
-		g.fillOval(destX * cellWidth + 4, destY * cellHeight + 4, 8, 8);
-
-		// Draw the path
-		g.setColor(new Color(255, 255, 240));
-		for (int i = 0; i < pathX.size(); i++)
-		{
-			g.fillOval(pathX.get(i) * cellWidth + 4, pathY.get(i) * cellHeight + 8, 8, 8);
-		}
+	}
+	
+	/**
+	 * main
+	 */
+	public static void main(String[] args)
+	{
+		new AntAStar(0,1,1,3).getPath();
 	}
 }
